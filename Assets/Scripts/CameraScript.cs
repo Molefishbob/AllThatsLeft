7ee9b70cca5353cff6fcb4b2d30244fd032652d4
Ville Heikkinen <heivil88@gmail.com>
2019-01-23 20:53:49 +0200

    Added jump and camera rotation

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float _verticalSensitivity;
+    public float _horizontalSensitivity;
+
+    private float _yaw = 0.0f;
+    private float _pitch = 0.0f;
+
+
+    void Update()
+    {
+        _yaw -= _horizontalSensitivity * Input.GetAxis("Mouse Y");
+        _pitch += _verticalSensitivity * Input.GetAxis("Mouse X");
+       
+        transform.eulerAngles = new Vector3(_yaw, _pitch, 0.0f);
+    }
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private bool _isGrounded;
+    private CharacterController _controller;
+    public float _jumpForce;
+    public float _gravity;
+    private Vector3 _jump = Vector3.zero;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        
+    }
+   
+    private void Update()
+    {
+
+        if (_isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _jump.y = _jumpForce;
+                _isGrounded = false;
+            }
+        }
+
+        _jump.y = _jump.y - (_gravity * Time.deltaTime);
+
+        _controller.Move(_jump * Time.deltaTime);
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.gameObject.layer == 9)
+        {
+            _isGrounded = true;
+        }
+    }
+}
