fcb2539d6ff8747ccd891297e59a542947e3696e
faanlez <zelfana@gmail.com>
2019-01-24 14:18:46 +0200

    added Singleton base class and game manager extending it

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    // (Optional) Prevent non-singleton constructor use.
+    protected GameManager() { }
+}
