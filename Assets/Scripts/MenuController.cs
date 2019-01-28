8cf536d1b3516444f3869e7ee01d4986ba473261
VoidCooper <santeri.sivula@tuni.fi>
2019-01-28 13:24:55 +0200

    Some functionality (Panel Changes)
    TODO: confirm Exit and scene switches

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableMainMenuPanel()
+    {
+        Canvas.transform.GetChild(1).gameObject.SetActive(true); // Main Menu panel
+        Canvas.transform.GetChild(2).gameObject.SetActive(false); // Options panel
+    }
+
+    public void EnableOptionsPanel()
+    {
+        Canvas.transform.GetChild(1).gameObject.SetActive(false); // Main Menu panel
+        Canvas.transform.GetChild(2).gameObject.SetActive(true); // Options panel
+    }
+}
