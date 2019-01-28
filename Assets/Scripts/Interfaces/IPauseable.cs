2057888960bf8d6666150c2fce3e030875d3a8c2
Teijo Lukkarinen <zelfana@gmail.com>
2019-01-28 18:09:16 +0200

    added comments to pause interface

--- Assets/Scripts/Interfaces/IPauseable.cs
+++ Assets/Scripts/Interfaces/IPauseable.cs
@@ -1,6 +1,12 @@
 public interface IPauseable {
     void Pause();
     void UnPause();
+    /// <summary>
+    /// Put this method in Start().
+    /// </summary>
     void AddToPauseCollection();
+    /// <summary>
+    /// Put this method in OnDestroy().
+    /// </summary>
     void RemoveFromPauseCollection();
 }
