b277e56893057c6aa8d6add0d0f6e7f0ad5ef89b
Teijo Lukkarinen <zelfana@gmail.com>
2019-01-28 16:22:40 +0200

    sound manager added

--- Assets/Scripts/Manager/VolumeControl.cs
+++ Assets/Scripts/Manager/VolumeControl.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
 
-public abstract class VolumeControl
-{
-    protected AudioSource audioSource;
-    private float fullVolume;
-    protected void Start() {
-        fullVolume = audioSource.volume;
+public abstract class VolumeControl {
+    private enum SoundType {
+        SoundEffect,
+        Music,
+        Interface
     }
-    void SetVolume(float volume) {
-
+    protected AudioSource _audioSource;
+    private float _fullVolume;
+    [SerializeField]
+    private SoundType _soundType = SoundType.SoundEffect;
+    protected virtual void Start() {
+        _fullVolume = _audioSource.volume;
+        switch(_soundType) {
+            case SoundType.SoundEffect:
+                SoundManager.Instance._soundsSFX.Add(this);
+                _audioSource.mute = SoundManager.Instance.MutedSFX || SoundManager.Instance.MutedAll;
+                _audioSource.volume = SoundManager.Instance.VolumeSFX * SoundManager.Instance.VolumeAll * _fullVolume;
+                break;
+            case SoundType.Music:
+                SoundManager.Instance._soundsMusic.Add(this);
+                _audioSource.mute = SoundManager.Instance.MutedMusic || SoundManager.Instance.MutedAll;
+                _audioSource.volume = SoundManager.Instance.VolumeMusic * SoundManager.Instance.VolumeAll * _fullVolume;
+                break;
+            case SoundType.Interface:
+                SoundManager.Instance._soundsUI.Add(this);
+                _audioSource.mute = SoundManager.Instance.MutedUI || SoundManager.Instance.MutedAll;
+                _audioSource.volume = SoundManager.Instance.VolumeUI * SoundManager.Instance.VolumeAll * _fullVolume;
+                break;
+            default:
+            Debug.LogError("INVALID SOUND TYPE!!!!!");
+                break;
+        }
     }
-    void Mute(bool muted) {
-
+    public void SetVolume(float volume) {
+        _audioSource.volume = volume * _fullVolume;
+    }
+    public void Mute(bool muted) {
+        _audioSource.mute = muted;
+    }
+    protected virtual void OnDestroy() {
+        switch(_soundType) {
+            case SoundType.SoundEffect:
+                SoundManager.Instance._soundsSFX.Remove(this);
+                break;
+            case SoundType.Music:
+                SoundManager.Instance._soundsMusic.Remove(this);
+                break;
+            case SoundType.Interface:
+                SoundManager.Instance._soundsUI.Remove(this);
+                break;
+            default:
+            Debug.LogError("INVALID SOUND TYPE!!!!!");
+                break;
+        }
     }
 }
