8cbe325d12d82f6bc1f785ee6f105a0ddf3f56f5
Teijo Lukkarinen <zelfana@gmail.com>
2019-01-28 16:20:02 +0200

    vscode formatting for game manager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
-{
+public class GameManager : Singleton<GameManager> {
     // (Optional) Prevent non-singleton constructor use.
     protected GameManager() { }
 }
