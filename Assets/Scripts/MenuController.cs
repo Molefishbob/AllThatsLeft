de425e897806d8221ed3718183ceac3d6b9f8540
Santeri Sivula <santeri.sivula@cs.tamk.fi>
2019-01-31 14:25:34 +0200

    Basic Menu functionality done

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,34 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField]
     private GameObject Canvas;
+    private GameObject mainMenuPanel;
+    private GameObject optionsPanel;
+    private GameObject quitPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenuPanel = Canvas.transform.GetChild(1).gameObject;
+        optionsPanel = Canvas.transform.GetChild(2).gameObject;
+        quitPanel = Canvas.transform.GetChild(3).gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void EnableMainMenuPanel()
     {
-        Canvas.transform.GetChild(1).gameObject.SetActive(true); // Main Menu panel
-        Canvas.transform.GetChild(2).gameObject.SetActive(false); // Options panel
+        mainMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        quitPanel.SetActive(false);
     }
 
     public void EnableOptionsPanel()
     {
-        Canvas.transform.GetChild(1).gameObject.SetActive(false); // Main Menu panel
-        Canvas.transform.GetChild(2).gameObject.SetActive(true); // Options panel
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void EnableConfirmQuit(){
+        mainMenuPanel.SetActive(false);
+        quitPanel.SetActive(true);
     }
 
     public void Quit()
