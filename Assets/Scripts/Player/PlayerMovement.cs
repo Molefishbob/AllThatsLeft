a4d0c494fc61630bd0475e4984c48526f65ee6ab
Ville Heikkinen <heivil88@gmail.com>
2019-02-04 15:06:07 +0200

    mistake fixing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,6 +14,9 @@ public class PlayerMovement : MonoBehaviour
     private static float _yaw = 0.0f;
     private CameraScript _camera;
     private Transform _trans;
+    private bool _canJump;
+    public float _jumpForce;
+    private Vector3 _jump = Vector3.zero;
 
     public static float Yaw
     {
@@ -48,23 +51,45 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-        /*Vector3 forward = vertical * _speed * Vector3.forward;*/
+        Vector3 forward = vertical * _speed * Vector3.forward;
 
         if (vertical < 0)
         {
-            //forward *= _backwardMultiplier;
+            forward *= _backwardMultiplier;
             vertical *= _backwardMultiplier;
         }
 
-        _trans.position += transform.forward * _speed * vertical * Time.deltaTime;
+        //_trans.position += transform.forward * _speed * vertical * Time.deltaTime;
 
-        //Vector3 sideWays = _strafeSpeed * horizontal * Vector3.right;
-        _trans.position += transform.right * _strafeSpeed * horizontal * Time.deltaTime;
+        Vector3 sideWays = _strafeSpeed * horizontal * Vector3.right;
+        //_trans.position += transform.right * _strafeSpeed * horizontal * Time.deltaTime;
 
-        //Vector3 movement = (forward + sideWays) * Time.deltaTime;
+        Vector3 movement = (forward + sideWays) * Time.deltaTime;
         
-        //movement = transform.TransformDirection(movement);
-        //Debug.Log(movement);
-        //_controller.Move(movement);
+        movement = transform.TransformVector(movement);
+      
+
+        _controller.Move(movement);
+
+        if (_canJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _jump.y = _jumpForce;
+                _canJump = false;
+            }
+        }
+
+        _jump += (Physics.gravity * Time.deltaTime);
+
+        _controller.Move(_jump * Time.deltaTime);
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        if (col.gameObject.layer == 9 || col.gameObject.layer == 10)
+        {
+            _canJump = true;
+        }
     }
 }
