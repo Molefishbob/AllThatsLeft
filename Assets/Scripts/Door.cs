353815b91f6b7261731d43beb06cea348cd58619
Finland to Sami <sami.sipari@tuni.fi>
2019-02-07 11:25:17 +0200

    Door added

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IButtonInteraction
+{
+    private Animator _anim;
+
+    // Awake is called before the first frame update
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+    
+    public void ButtonDown()
+    {
+        _anim.SetTrigger("Open");
+    }
+
+    public void ButtonUp()
+    {
+        _anim.SetTrigger("Close");
+    }
+}
