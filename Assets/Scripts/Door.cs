eac8b59166a49ab782f102b7555eaed8e1856a89
Sami Sipari <sami.sipari@cs.tamk.fi>
2019-02-10 12:05:05 +0200

    Fine tuned the pressure plate and added prefabs for it and the door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IButtonInteraction
 {
+    [Tooltip("The name of the 'opening' trigger parameter in the animator")]
     public string _openTrigger = "Open";
+    [Tooltip("The name of the 'closing' trigger parameter in the animator")]
     public string _closeTrigger = "Close";
     private Animator _anim;
 
