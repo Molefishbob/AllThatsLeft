da146bde16e83063b98c62006baa8e38e58ced85
Teijo Lukkarinen <zelfana@gmail.com>
2019-02-22 23:30:22 +0200

    bot deploy continued

--- Assets/Scripts/Player/DeployBots.cs
+++ Assets/Scripts/Player/DeployBots.cs
@@ -8,15 +8,32 @@ public class DeployBots : MonoBehaviour, IPauseable
     public string _selectBot1Button = "Bot1";
     public string _selectBot2Button = "Bot2";
     public string _selectBot3Button = "Bot3";
+    //public string _selectBot4Button = "Bot4";
+    //public string _selectBot5Button = "Bot5";
     public string _deployBotButton = "Deploy Bot";
+    public float _buttonDelay = 0.1f;
+    public Transform _attachPoint;
     public Transform _deployTarget;
     public float _extraSpaceRequired;
     public LayerMask _deployCollisionLayers;
     public LayerMask _deployableTerrain;
+    public int _startingBotAmount;
 
     private bool _paused;
     private int _selectedBot;
+    private int _numTypes = 3;
     private CharControlBase _heldBot; //TODO: change this to base bot type
+
+    public int BotAmount
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        BotAmount = _startingBotAmount;
+    }
 
     private void Start()
     {
@@ -51,8 +68,10 @@ public class DeployBots : MonoBehaviour, IPauseable
                     {
                         _heldBot.transform.parent = null;
                         _heldBot.transform.position = _deployTarget.position;
-                        //_heldBot.activate
+                        _heldBot.transform.rotation = _deployTarget.rotation;
+                        //_heldBot.activate();
                         _heldBot = null;
+                        BotAmount--;
                     }
                 }
                 else
@@ -62,7 +81,42 @@ public class DeployBots : MonoBehaviour, IPauseable
             }
             else
             {
-                // TODO: hide indicator
+                if (Input.GetButtonDown(_selectBot1Button))
+                {
+                    _selectedBot = 0;
+                }
+                else if (Input.GetButtonDown(_selectBot2Button))
+                {
+                    _selectedBot = 1;
+                }
+                else if (Input.GetButtonDown(_selectBot3Button))
+                {
+                    _selectedBot = 2;
+                }
+                /* else if (Input.GetButtonDown(_selectBot4Button))
+                {
+                    _selectedBot = 3;
+                }
+                else if (Input.GetButtonDown(_selectBot5Button))
+                {
+                    _selectedBot = 4;
+                } */
+                else if (Input.GetAxisRaw(_scrollButton) > 0.0f)
+                {
+                    _selectedBot++;
+                    if (_selectedBot >= _numTypes)
+                    {
+                        _selectedBot = 0;
+                    }
+                }
+                else if (Input.GetAxisRaw(_scrollButton) < 0.0f)
+                {
+                    _selectedBot--;
+                    if (_selectedBot < 0)
+                    {
+                        _selectedBot = _numTypes - 1;
+                    }
+                }
             }
         }
     }
