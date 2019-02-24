e20dc38fb7a75199f9b6ceb7745e08d1c5a355b7
Ville Heikkinen <heivil88@gmail.com>
2019-02-24 18:47:01 +0200

    Frog enemy almost moves like it's supposed to.
    But not quite

--- Assets/Scripts/Enemies/FrogEnemy.cs
+++ Assets/Scripts/Enemies/FrogEnemy.cs
@@ -2,21 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogEnemy : CharControlBase
+public class FrogEnemy : CharControlBase, ITimedAction
 {
-    private float _time;
+    private float _time = 0;
+    public float _circleRadius = 1;
+    private bool _stopMoving = false;
+    private OneShotTimer _timer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _timer = GetComponent<OneShotTimer>();
+        _timer.SetTimerTarget(this);
+    }
+
 
     protected override Vector3 InternalMovement()
     {
-        //just testing things for now
-        _time += Time.deltaTime * 5;
-        
-        float x = Mathf.Sin(_time) * 10;
-        float y = 0;
-        float z = Mathf.Cos(_time) * 5;
-        
+
+        float x, y, z;
+
+        if (!_stopMoving)
+        {
+            _time += Time.deltaTime / _circleRadius;
+
+            x = Mathf.Sin(_time);
+            y = 0;
+            z = Mathf.Cos(_time);
+        }
+        else
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        if(x > 0.9999f || x < -0.9999f || z > 0.9999f || z < -0.9999f)
+        {
+            _stopMoving = true;
+            _timer.StartTimer(2);
+        }
+
         Vector3 move = new Vector3(x,y,z);
-        Debug.Log(move);
+        Debug.Log(x);
+        
         return move;
-    } 
+    }
+
+    public void TimedAction()
+    {
+        _stopMoving = false;
+    }
 }
