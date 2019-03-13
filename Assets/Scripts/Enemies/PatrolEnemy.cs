59a1708454bbbb1727240858fd98dd9d48fe141a
Ville Heikkinen <heivil88@gmail.com>
2019-03-13 12:51:03 +0200

    Added enemypools to level manager and references to gamemanager

--- Assets/Scripts/Enemies/PatrolEnemy.cs
+++ Assets/Scripts/Enemies/PatrolEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolEnemy : MonoBehaviour
+public class PatrolEnemy : PatrolEnemyMover
 {
     //This is only here so the PatrolEnemyPool can use the PatrolEnemy prefab
     //Actual behaviour scripts are in the prefabs child components
