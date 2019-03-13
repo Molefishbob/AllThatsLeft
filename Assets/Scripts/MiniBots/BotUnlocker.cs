190bdf9096338af085dac11e739981c1f8f8602c
Teijo Lukkarinen <zelfana@gmail.com>
2019-03-13 14:43:51 +0200

    finished code and prefabs for bot unlockers, replace temp models next

--- Assets/Scripts/MiniBots/BotUnlocker.cs
+++ Assets/Scripts/MiniBots/BotUnlocker.cs
@@ -6,10 +6,20 @@ public class BotUnlocker : MonoBehaviour
 {
     [SerializeField]
     private MiniBotType _bot = MiniBotType.HackBot;
+    [SerializeField]
+    private bool _restockBots = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        GetComponentInChildren<DeployBots>().UnlockBot(_bot);
+        DeployBots dep = other.GetComponentInChildren<DeployBots>();
+
+        dep.UnlockBot(_bot);
+
+        if (_restockBots)
+        {
+            dep.Restock();
+        }
+
         gameObject.SetActive(false);
     }
 }
