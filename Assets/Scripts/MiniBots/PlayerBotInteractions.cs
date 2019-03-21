537bb696765fb67d16991511d408a5fb3fa4e6ff
Santeri Sivula <santeri.sivula@tuni.fi>
2019-03-21 14:49:16 +0200

    BotMovement and interaction continued

--- Assets/Scripts/MiniBots/PlayerBotInteractions.cs
+++ Assets/Scripts/MiniBots/PlayerBotInteractions.cs
@@ -9,26 +9,51 @@ public class PlayerBotInteractions : MonoBehaviour
     [SerializeField]
     private float _fExplodeRadius = 4;
     public LayerMask _lHackableLayer = 1 << 18;
-    public LayerMask _lBombableLayer = 1 << 11 | 1 << 10;
+    public LayerMask _lBombableLayer = 1 << 11 | 1 << 10 | 1 << 9;
     [SerializeField]
-    private string _sHackButton = "Use Object";
+    private string _sHackButton = "Hack Action";
     [SerializeField]
-    private string _sExplodeButton = "Cancel";
-    public bool _bActive = false;
+    private string _sExplodeButton = "Bomb Action";
+    [SerializeField]
+    private string _sStayButton = "Stay Action";
+    public bool _bActive
+    {
+        get { return _bactive; }
+        set
+        {
+            _bactive = value;
+            _selfMover.ControlsDisabled = !value;
+            _selfMover.SetControllerActive(value);
+        }
+    }
+    private bool _bactive = false;
+        [SerializeField]
     private bool _bActing = false;
+    private bool _bReleasing = false;
+    private int _iReleaseDelay = 2;
+    private int _iReleaseDelayCounter = 0;
     private GameObject[] _goTarget = null;
+    private PlayerMovement _selfMover;
 
-    // Read inputs here
+    void Awake()
+    {
+        _selfMover = GetComponent<PlayerMovement>();
+    }
+
     void Update()
     {
         if (Input.GetButtonDown(_sHackButton) && _bActive && !_bActing)
-        { 
+        {
             _goTarget = CheckSurroundings(_lHackableLayer, false);
             if (_goTarget != null)
             {
                 GenericHackable ghOther = _goTarget[0].GetComponent<GenericHackable>();
                 if (ghOther._currentStatus == GenericHackable.Status.NotHacked)
+                {
+                    _bActing = true;
                     ghOther.TimeToStart();
+                    ReleaseControls();
+                }
             }
         }
 
@@ -39,10 +64,25 @@ public class PlayerBotInteractions : MonoBehaviour
             {
                 foreach (GameObject o in _goTarget)
                 {
-                    o.SetActive(false);
+                    o.gameObject.GetComponent<IDamageReceiver>()?.TakeDamage(1);
                 }
+                ReleaseControls();
             }
         }
+
+        if (_bReleasing)
+        {
+            if (_iReleaseDelayCounter >= _iReleaseDelay)
+            {
+                GameManager.Instance.Player.ControlsDisabled = !_bReleasing;
+                _iReleaseDelayCounter = 0;
+                _bReleasing = !_bReleasing;
+            }
+            _iReleaseDelayCounter++;
+        } 
+
+        if (Input.GetButtonDown(_sStayButton) && _bActive)
+            ReleaseControls();
     }
 
     private GameObject[] CheckSurroundings(LayerMask interLayer, bool isExplosion)
@@ -52,7 +92,7 @@ public class PlayerBotInteractions : MonoBehaviour
         {
             hitColliders = Physics.OverlapSphere(transform.position, _fDetectRadius, interLayer);
         }
-        else 
+        else
         {
             hitColliders = Physics.OverlapSphere(transform.position, _fExplodeRadius, interLayer);
         }
@@ -71,7 +111,6 @@ public class PlayerBotInteractions : MonoBehaviour
         {
             int closestIndex = 0;
             GameObject[] tmpObject = new GameObject[hitColliders.Length];
-            _bActing = !isExplosion;
             for (int i = 0; i < hitColliders.Length; i++)
             {
                 if (hitColliders[i] == closestObject)
@@ -79,6 +118,7 @@ public class PlayerBotInteractions : MonoBehaviour
                     closestIndex = i;
                 }
                 tmpObject[i] = hitColliders[i].gameObject;
+                Debug.DrawLine(transform.position, tmpObject[i].transform.position,Color.red, 2f);
             }
             if (closestIndex != 0)
             {
@@ -88,5 +128,24 @@ public class PlayerBotInteractions : MonoBehaviour
             return tmpObject;
         }
         return null;
+    }
+
+    // Release the controls back to the player
+    public void ReleaseControls()
+    {
+        _selfMover.ControlsDisabled = true;
+        _bReleasing = true;
+        GameManager.Instance.Camera.GetNewTarget(GameManager.Instance.Player.transform);
+    }
+
+    public void StopActing()
+    {
+        if (_bActing)
+        {
+            _bActing = false;
+            Debug.Log(_goTarget[0].GetComponent<GenericHackable>());
+            _goTarget[0].GetComponent<GenericHackable>()?.TimeToLeave();
+            _goTarget = null;
+        }
     }
 }
