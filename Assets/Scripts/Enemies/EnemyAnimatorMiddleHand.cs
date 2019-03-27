fd7a86675c5974da5b57cec17e5e7cb7accd40e0
Santeri Sivula <santeri.sivula@cs.tamk.fi>
2019-03-27 12:27:40 +0200

    FrogHopsound
    Please use other sounds

---
+++ Assets/Scripts/Enemies/EnemyAnimatorMiddleHand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimatorMiddleHand : MonoBehaviour
+{
+    [SerializeField]
+    private RandomSFXSound _hopsfx;
+
+    public void PlayHopSound()
+    {
+        _hopsfx.PlaySound(true);
+    }
+}
