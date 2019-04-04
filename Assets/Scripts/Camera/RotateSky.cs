1267fe7d1c3483424f0623ac19554ca830b640dc
Teijo Lukkarinen <zelfana@gmail.com>
2019-04-04 17:18:59 +0300

    fixed material changing when skybox rotates

--- Assets/Scripts/Camera/RotateSky.cs
+++ Assets/Scripts/Camera/RotateSky.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class RotateSky : MonoBehaviour
 {
+    public Material _bgMat;
 
     public float RotateSpeed = 1.2f;
-   
+   private void Awake() {
+        RenderSettings.skybox = Instantiate(_bgMat);
+    }
     void Update()
     {
 
