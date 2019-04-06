36b5a6086dc14a6bdc1463961951204508bbe6e0
Teijo Lukkarinen <zelfana@gmail.com>
2019-04-06 20:46:13 +0300

    added some time in loading screen before transition starts

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -5,17 +5,70 @@ using UnityEngine;
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _ball;
+    private Transform _scaledObject = null;
+    [SerializeField]
+    private float _startScale = 0f;
+    [SerializeField]
+    private float _endScale = 1f;
+    [SerializeField]
+    private float _timeBeforeTransition = 1f;
+    [SerializeField]
+    private float _transitionDuration = 2f;
+
+    private UnscaledOneShotTimer _timer;
+    private bool _mute;
+    private bool _inTransition;
 
     private void Awake()
     {
+        _timer = gameObject.AddComponent<UnscaledOneShotTimer>();
     }
 
     private void OnEnable()
     {
+        _scaledObject.localScale = Vector3.zero;
+
+        _mute = PrefsManager.Instance.AudioMuteSFX;
+        PrefsManager.Instance.AudioMuteSFX = true;
+
         if (GameManager.Instance.Player != null)
             GameManager.Instance.Player.ControlsDisabled = true;
 
-        PrefsManager.Instance.AudioMuteSFX = true;
+        _inTransition = false;
+
+        _timer.OnTimerCompleted += GrowUp;
+        _timer.StartTimer(_timeBeforeTransition);
+    }
+
+    void Update()
+    {
+        if (_inTransition)
+        {
+            float currentScale = _timer.NormalizedTimeElapsed * (_endScale - _startScale) + _startScale;
+            _scaledObject.localScale = Vector3.one * currentScale;
+        }
+    }
+
+    private void GrowUp()
+    {
+        _timer.OnTimerCompleted -= GrowUp;
+
+        _scaledObject.localScale = Vector3.one * _startScale;
+
+        PrefsManager.Instance.AudioMuteSFX = _mute;
+
+        if (GameManager.Instance.Player != null)
+            GameManager.Instance.Player.ControlsDisabled = false;
+
+        _inTransition = true;
+
+        _timer.OnTimerCompleted += EndMe;
+        _timer.StartTimer(_transitionDuration);
+    }
+
+    private void EndMe()
+    {
+        _timer.OnTimerCompleted -= EndMe;
+        gameObject.SetActive(false);
     }
 }
