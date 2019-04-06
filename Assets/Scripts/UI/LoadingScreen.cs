dd8844015220e9c27757a0a05737854313ab5bc8
Sami Sipari <sami.sipari@cs.tamk.fi>
2019-04-06 13:34:07 +0300

    Loading screen reworked with shaders

--- Assets/Scripts/UI/LoadingScreen.cs
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -5,33 +5,17 @@ using UnityEngine;
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField]
-    private float _extraLoadingTime = 2.0f;
-    private UnscaledOneShotTimer _timer;
-    private bool _mute;
+    private GameObject _ball;
 
     private void Awake()
     {
-        _timer = gameObject.AddComponent<UnscaledOneShotTimer>();
-        _timer.OnTimerCompleted += LoadingDone;
-    }
-
-    private void OnDestroy()
-    {
-        if (_timer != null) _timer.OnTimerCompleted -= LoadingDone;
     }
 
     private void OnEnable()
     {
-        GameManager.Instance.Player.ControlsDisabled = true;
-        _mute = PrefsManager.Instance.AudioMuteSFX;
-        PrefsManager.Instance.AudioMuteSFX = true;
-        _timer.StartTimer(_extraLoadingTime);
-    }
+        if (GameManager.Instance.Player != null)
+            GameManager.Instance.Player.ControlsDisabled = true;
 
-    private void LoadingDone()
-    {
-        GameManager.Instance.Player.ControlsDisabled = false;
-        PrefsManager.Instance.AudioMuteSFX = _mute;
-        gameObject.SetActive(false);
+        PrefsManager.Instance.AudioMuteSFX = true;
     }
 }
---
+++ Assets/Scripts/UI/UpwardsScale.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpwardsScale : MonoBehaviour
+{
+    [SerializeField]
+    private float _startScale = 0f;
+    [SerializeField]
+    private float _endScale = 1f;
+    [SerializeField]
+    private float _duration = 2f;
+    [SerializeField, Tooltip("Disables this object if defined after the timer has runout")]
+    private GameObject _disableObject = null;
+
+    private UnscaledOneShotTimer _timer;
+    private bool _timerComplete = false;
+    private bool _mute;
+    private float _currentScale;
+    private const int Count = 120;
+    private float _dx;
+
+    
+    private void OnEnable()
+    {
+
+        _timer = gameObject.AddComponent<UnscaledOneShotTimer>();
+        _timer.StartTimer(_duration);
+        _timer.OnTimerCompleted += TimerComplete;
+        _mute = PrefsManager.Instance.AudioMuteSFX;
+        transform.localScale = new Vector3(_startScale, _startScale, _startScale);
+        _currentScale = _startScale;
+        _dx = (_endScale - _startScale) / Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentScale += _dx;
+        if (!_timerComplete && _currentScale < _endScale)
+        {
+            transform.localScale = Vector3.one * _currentScale;
+        } else
+        {
+            transform.localScale = new Vector3(_endScale, _endScale, _endScale);
+        }
+
+        if (_timer.TimeElapsed > _timer.TimeElapsed * 0.25f)
+        {
+            if (GameManager.Instance.Player != null)
+                GameManager.Instance.Player.ControlsDisabled = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        _timer.OnTimerCompleted -= TimerComplete;
+        _timerComplete = false;
+        PrefsManager.Instance.AudioMuteSFX = _mute;
+        _currentScale = _startScale;
+
+    }
+    private void TimerComplete()
+    {
+        _timerComplete = true;
+        transform.localScale = new Vector3(_startScale, _startScale, _startScale);
+        _disableObject.SetActive(false);
+    }
+}
