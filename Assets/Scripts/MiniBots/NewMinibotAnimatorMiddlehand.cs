091b3a96eae8714ee32e8ebc49dea8e07f6bcbf3
Santeri Sivula <santeri.sivula@cs.tamk.fi>
2019-04-07 19:41:39 +0300

    BotReleasingis now littlebit smarter

--- Assets/Scripts/MiniBots/NewMinibotAnimatorMiddlehand.cs
+++ Assets/Scripts/MiniBots/NewMinibotAnimatorMiddlehand.cs
@@ -7,10 +7,12 @@ public class NewMinibotAnimatorMiddlehand : MonoBehaviour
     private BombAction _bombAction = null;
     [SerializeField]
     private RandomSFXSound _WalkSound = null;
+    private HackAction _hackAction = null;
 
     void Awake()
     {
         _bombAction = transform.parent.gameObject.GetComponent<BombAction>();
+        _hackAction = transform.parent.gameObject.GetComponent<HackAction>();
     }
 
     public void Explode()
@@ -21,5 +23,10 @@ public class NewMinibotAnimatorMiddlehand : MonoBehaviour
     public void PlayStepSound()
     {
         _WalkSound.PlaySound(true);
+    }
+
+    public void CheckIfHackDone()
+    {
+        _hackAction.CheckHackDone();
     }
 }
