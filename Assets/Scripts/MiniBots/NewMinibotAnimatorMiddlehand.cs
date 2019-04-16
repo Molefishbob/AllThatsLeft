06fc13c8c3572db7d8886fb2bc4d6004666ee060
Teijo Lukkarinen <zelfana@gmail.com>
2019-04-17 00:08:07 +0300

    some cleanup and made minibots play random idle animations

--- Assets/Scripts/MiniBots/NewMinibotAnimatorMiddlehand.cs
+++ Assets/Scripts/MiniBots/NewMinibotAnimatorMiddlehand.cs
@@ -7,10 +7,18 @@ public class NewMinibotAnimatorMiddlehand : MonoBehaviour
     private BombAction _bombAction = null;
     [SerializeField]
     private RandomSFXSound _WalkSound = null;
+    [SerializeField, Tooltip("Percentage")]
+    private float _randomIdleChance = 10.0f;
+    [SerializeField]
+    private string _rngTrigger1 = "RNG1";
+    [SerializeField]
+    private string _rngTrigger2 = "RNG2";
+    private Animator _animator;
 
     void Awake()
     {
         _bombAction = transform.parent.gameObject.GetComponent<BombAction>();
+        _animator = GetComponent<Animator>();
     }
 
     public void Explode()
@@ -21,5 +29,14 @@ public class NewMinibotAnimatorMiddlehand : MonoBehaviour
     public void PlayStepSound()
     {
         _WalkSound.PlaySound(true);
+    }
+
+    public void IdleCycle()
+    {
+        if (Random.Range(1, 101) <= _randomIdleChance)
+        {
+            if (Random.Range(0, 2) == 0) _animator.SetTrigger(_rngTrigger1);
+            else _animator.SetTrigger(_rngTrigger2);
+        }
     }
 }
