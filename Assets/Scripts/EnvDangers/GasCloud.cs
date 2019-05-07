66aaad34d86d33e1b59e65f3247a07dac088a0f2
Teijo Lukkarinen <zelfana@gmail.com>
2019-05-07 20:00:18 +0300

    added particle resizers for gas and electric traps

--- Assets/Scripts/EnvDangers/GasCloud.cs
+++ Assets/Scripts/EnvDangers/GasCloud.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class GasCloud : MonoBehaviour
 {
     [SerializeField, Tooltip("Time until the unit dies inside the area")]
@@ -13,26 +12,10 @@ public class GasCloud : MonoBehaviour
     private float _deathTintStrength = 0.75f;
     [SerializeField]
     private SingleSFXSound _gasSound = null;
-    [SerializeField]
-    private float _effectOverflowAmount = 1.0f;
-    [SerializeField]
-    private float _particleDensity = 1.0f;
 
     private void Start()
     {
         _gasSound.PlaySound();
-
-    }
-
-    private void LateUpdate() {
-        BoxCollider box = GetComponent<BoxCollider>();
-        ParticleSystem[] effects = GetComponentsInChildren<ParticleSystem>(true);
-        foreach (ParticleSystem effect in effects)
-        {
-            ParticleSystem.ShapeModule shape = effect.shape;
-            shape.position = box.center;
-            shape.scale = box.size + Vector3.one * _effectOverflowAmount;
-        }
     }
 
     private void OnTriggerEnter(Collider other)
