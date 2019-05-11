6964996d2f52ab6c500cf108c12d5f452ab6eb4a
Teijo Lukkarinen <zelfana@gmail.com>
2019-05-12 00:41:22 +0300

    tramp bot forces player to stay moving with the same platform it is on

--- Assets/Scripts/MiniBots/TopOfThetramp.cs
+++ Assets/Scripts/MiniBots/TopOfThetramp.cs
@@ -7,11 +7,14 @@ public class TopOfThetramp : MonoBehaviour
     public float _fJumpHeight = 5;
     private PlayerJump _PlayerMover;
     private MainCharMovement _Player;
+    private CharControlPlatformMovement _PlayerCCPM;
+    private CharControlPlatformMovement _ownCCPM;
     private Animator _animator;
 
     void Awake()
     {
         _animator = transform.parent.gameObject.GetComponentInChildren<Animator>();
+        _ownCCPM = transform.parent.GetComponent<CharControlPlatformMovement>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -20,11 +23,13 @@ public class TopOfThetramp : MonoBehaviour
         {
             _PlayerMover = other.GetComponent<PlayerJump>();
             _Player = other.GetComponent<MainCharMovement>();
+            _PlayerCCPM = other.GetComponent<CharControlPlatformMovement>();
         }
         if (!_Player.IsGrounded && !_Player.ControlsDisabled)
         {
             _animator.SetTrigger("TrampolineBounce");
             _PlayerMover.ForceJump(_fJumpHeight);
+            if (_ownCCPM._platform != null) _PlayerCCPM.ForcePlatform(_ownCCPM._platform);
         }
     }
 }
