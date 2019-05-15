30906f78d3e215a13df61d5dfc6ebaf7025f753e
Ville Heikkinen <heivil88@gmail.com>
2019-05-15 15:08:48 +0300

    working on scorpion teleport effect

--- Assets/Scripts/Enemies/ScorpionSpawner.cs
+++ Assets/Scripts/Enemies/ScorpionSpawner.cs
@@ -10,7 +10,10 @@ public class ScorpionSpawner : MonoBehaviour
     [SerializeField]
     private float _speed = 3;
     public float _respawnTime = 10;
+    public float _teleportTime = 2;
     private ScaledOneShotTimer _timer;
+    private ScaledOneShotTimer _shaderTimer;
+    private int _shaderProperty;
 
     public List<Transform> Targets
     {
@@ -20,16 +23,34 @@ public class ScorpionSpawner : MonoBehaviour
     private void Awake()
     {
         _timer = gameObject.AddComponent<ScaledOneShotTimer>();
+        _shaderTimer = gameObject.AddComponent<ScaledOneShotTimer>();
         _timer.OnTimerCompleted += Spawn;
+        _shaderTimer.OnTimerCompleted += TeleportComplete;
+        _shaderProperty = Shader.PropertyToID("_cutoff");
         foreach (Transform child in transform)
         {
             _patrolTargets.Add(child);
         }
     }
 
+    private void Update()
+    {
+        if (_shaderTimer.IsRunning)
+        {
+            Debug.Log("aa");
+            _patrolEnemy._renderer.materials[1].SetFloat(_shaderProperty, _shaderTimer.NormalizedTimeElapsed);
+        }
+    }
+
+    private void OnDisable()
+    {
+        _patrolEnemy._renderer.materials[1].SetFloat(_shaderProperty, 0.0f);
+    }
+
     private void OnDestroy()
     {
         if (_timer != null) _timer.OnTimerCompleted -= Spawn;
+        if (_shaderTimer != null) _shaderTimer.OnTimerCompleted -= TeleportComplete;
     }
 
     public void Spawn()
@@ -39,7 +60,15 @@ public class ScorpionSpawner : MonoBehaviour
         _patrolEnemy.transform.position = transform.position;
         _patrolEnemy.Targets = _patrolTargets;
         _patrolEnemy._spawner = this;
+        _shaderTimer.StartTimer(_teleportTime);
+    }
+
+    private void TeleportComplete()
+    {
+        Debug.Log("ss");
         _patrolEnemy.SetControllerActive(true);
+        _patrolEnemy.gameObject.GetComponentInChildren<ScorpionTakeDamage>()._scorpion._renderer.materials[0].SetFloat(_shaderProperty, 0.0f);
+        _patrolEnemy._renderer.materials[1].SetFloat(_shaderProperty, 0.0f);
     }
 
     private void OnTriggerEnter(Collider other)
